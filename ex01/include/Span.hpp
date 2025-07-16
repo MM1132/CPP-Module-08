5bc792bedc722bf9496d06c9d1e36f8e0139dc88
Robert Reimann <rreimann@1-B-18.42heilbronn.de>
2025-07-16 19:41:52 +0200

    Different useful phrases in German

--- ex01/include/Span.hpp
+++ ex01/include/Span.hpp
@@ -5,5 +5,42 @@
 
 class Span
 {
-	
+	unsigned int m_N;
+
+	public:
+		Span(unsigned int N);
+		~Span() = default;
+
+		// Arschloch
+		// sch
+		// schwul
+		// ich kaufe dein schwules Arschloch
+		// deine Mutter kauft dein schwules Arschloch
+		// Deine Kinder kaufen dein schwules Arschloch im Mediamarkt
+		// Meine Kinder ficken ihre Mütter mit Pommes am Hauptbahnhof
+		// Lass uns Ping-Pong spielen! 
+		// Lasst uns im Mediamarkt ficken! 
+		// Lasst uns Tischtennis spielen und Kaffe drinken! 
+		// Kann ich bitte eine Besucherkarte haben? 
+		// Kann ich bitte deine Kinder ficken? 
+		// Lasst uns deine Kinder ficken, aber bitte mit Pommes! 
+		// Welches Projekt machst du gerade? 
+		// Lass mich durch dein Arschloch, bitte! 
+		// Ich gehe mit deinen Kindern in die Schule. 
+		// Gib mir deine Kinder, bitte! 
+		// Gib mir dein Geld, du Opfer! 
+		// Batman ist cooler als deine Mutter! 
+		// Englisch ist die beste Sprache im Hauptbahnhof, um deine Kinder zu verkaufen.
+		// Ich esse gerne Wassermelone in Minecraft. 
+		// Meine Mutter ist besser als deine Mutter. 
+		// Meine Mutter fickt deine Kinder besser als deine Mutter.
+		// Deine Mutter ist so fett wie tausend (1000) Avocados.
+		// Robert wiegt hundert Kilo mehr als Mischas Opa. 
+		// Kann ich mehr Powermeal haben als er, du Pimmel? 
+		// Kann ich mehr Arschloch haben, bitte? Ich bin voll schwul und geil!
+		// Ich sitze am Computer. 
+		// Lasst uns sofort zusammen deine Kinder ficken! 
+		// Jaa! Auf gehts Leute! 
+
+		void addNumber();
 };
---
+++ ex01/src/Span.cpp
@@ -0,0 +1,3 @@
+#include "Span.hpp"
+
+Span::Span(unsigned int N): m_N(N) {}
